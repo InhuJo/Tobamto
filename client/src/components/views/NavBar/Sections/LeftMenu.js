d34fe653f9000420cf75442e0adf0d9cf8145184
JUYEONGOH <ojy4475@naver.com>
2021-05-27 16:56:29 +0900

    nav bar color update

--- client/src/components/views/NavBar/Sections/LeftMenu.js
+++ client/src/components/views/NavBar/Sections/LeftMenu.js
@@ -6,16 +6,16 @@ function LeftMenu(props) {
   return (
     <Menu mode={props.mode}>
     <SubMenu title={<span>이용 안내</span>}>
-        <Menu.Item key="setting:1">▶ 이용 안내</Menu.Item>
-        <Menu.Item key="setting:2">▶ 개발자 소개</Menu.Item>
+        <Menu.Item key="setting:0">▶ 이용 안내</Menu.Item>
+        <Menu.Item key="setting:1">▶ 개발자 소개</Menu.Item>
     </SubMenu>
     <SubMenu title={<span>토론의 장</span>}>
-        <Menu.Item key="setting:1">▶ 진행중인 토론</Menu.Item>
-        <Menu.Item key="setting:2">▶ 지난 토론</Menu.Item>
-        <Menu.Item key="setting:3">▶ 토론 주제 신청</Menu.Item>
+        <Menu.Item key="setting:2">▶ 진행중인 토론</Menu.Item>
+        <Menu.Item key="setting:3">▶ 지난 토론</Menu.Item>
+        <Menu.Item key="setting:4">▶ 토론 주제 신청</Menu.Item>
     </SubMenu>
     <SubMenu title={<span>문의하기</span>}>
-        <Menu.Item key="setting:1">▶ 문의하기</Menu.Item>
+        <Menu.Item key="setting:5">▶ 문의하기</Menu.Item>
     </SubMenu>
   </Menu>
   )
