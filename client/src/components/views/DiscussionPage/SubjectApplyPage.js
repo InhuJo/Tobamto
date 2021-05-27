d62e82754183b0c0795c3f001d4ef3977eb6e87b
InhuJo <inhudev@gmail.com>
2021-05-27 16:59:21 +0900

    토론 주제 신청 페이지 생성

---
+++ client/src/components/views/DiscussionPage/SubjectApplyPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Discussion.css';
+
+function SubjectApplyPage() {
+    return (
+        <div>
+            <div className="top">
+                <h1 className="title" style={{ fontWeight: 600, marginLeft: 50 }}>새로 올라온 주제</h1>
+                <button className="writeBtn">주제 신청</button>
+            </div>
+            <div className="board">
+                
+            </div>
+        </div>
+    )
+}
+
+export default SubjectApplyPage
