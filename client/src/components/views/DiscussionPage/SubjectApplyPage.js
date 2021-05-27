d76cac31a260b2999f67bf67b5ded43b7444555c
InhuJo <inhudev@gmail.com>
2021-05-27 17:30:44 +0900

    토론 주제 신청 페이지: 표 추가

--- client/src/components/views/DiscussionPage/SubjectApplyPage.js
+++ client/src/components/views/DiscussionPage/SubjectApplyPage.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import {Table} from 'antd';
 import './Discussion.css';
 
 function SubjectApplyPage() {
+    const subjectList = [
+        {
+            key: '1',
+            title: '겨울에 얼어 죽어도 아아인가?',
+            writer: '엔젤빌',
+            date: '2021-05-25 21:19'
+        },
+        {
+            key: '2',
+            title: '롤할 쮸들 괌',
+            writer: '이누',
+            date: '2021-05-25 17:11'
+        }
+    ]
+
+    const columns = [
+        {
+            title: '제목',
+            dataIndex: 'title',
+            key: 'title'
+        },
+        {
+            title: '글쓴이',
+            dataIndex: 'writer',
+            key: 'writer'
+        },
+        {
+            title: '날짜',
+            dataIndex: 'data',
+            key: 'date'
+        }
+    ]
+
     return (
         <div>
             <div className="top">
@@ -9,7 +43,7 @@ function SubjectApplyPage() {
                 <button className="writeBtn">주제 신청</button>
             </div>
             <div className="board">
-                
+                <Table dataSource={subjectList} columns={columns} />
             </div>
         </div>
     )
