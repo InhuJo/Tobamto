b482f51ff5005c91f88f967f0b7024f005c6358d
JUYEONGOH <ojy4475@naver.com>
2021-05-27 17:33:04 +0900

    InformationPage

---
+++ client/src/components/views/InformationPage/Information.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { FaCode } from "react-icons/fa";
+
+function Information() {
+    return (
+       <div>
+           정보
+       </div>
+    )
+}
+
+export default Information
