5e60f453cd2dc252d8d68b35e76f5c2e75320a66
Juyoung Kim <nullyng@gmail.com>
2021-06-10 18:42:10 +0900

    edits something

--- client/src/components/views/DiscussionPage/OngoingDiscussionPage.js
+++ client/src/components/views/DiscussionPage/OngoingDiscussionPage.js
@@ -8,7 +8,7 @@ function OngoingDiscussionPage() {
         <div style={{ textAlign: 'center', marginTop: '100px', marginBottom: '50px' }}>
             <Title level={3} style={{ width: '100%' }}> 현재 진행중인 토론
             </Title>
-            <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center', flexDirection: 'row', flexWrap: 'wrap', width: '100%', minWidth: '60%', paddingLeft: '100px', paddingRight: '100px'}}>
+            <div className="ongoing-discussion-lists" style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center', flexDirection: 'row', flexWrap: 'wrap', width: '100%', minWidth: '60%', paddingLeft: '100px', paddingRight: '100px'}}>
                 <div className="ongoing-discussion" style={{ display: 'flex', alignItems: 'center', width: '200px', height:'150px', background: '#F0F0F0', padding: '1rem', flexWrap: 'wrap', justifyContent: 'center', marginTop: '1rem', marginBottom: '1rem', marginLeft: '0.5rem', marginRight: '0.5rem'}}>
                     <h3><strong> 안락사, 허용해야 한다 </strong></h3>
                     <br />
