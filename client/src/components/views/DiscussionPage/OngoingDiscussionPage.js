dbb907d529b20015cda6e6e0173725404532d12b
Juyoung Kim <nullyng@gmail.com>
2021-06-10 17:40:05 +0900

    edit OngoingDiscussionPage

--- client/src/components/views/DiscussionPage/OngoingDiscussionPage.js
+++ client/src/components/views/DiscussionPage/OngoingDiscussionPage.js
@@ -5,13 +5,15 @@ const { Title } = Typography;
 function OngoingDiscussionPage() {
 
      return (
-        <div style={{ textAlign: 'center' }}>
-            <Title level={3}> 현재 진행중인 토론
+        <div style={{ textAlign: 'center', marginTop: '100px', marginBottom: '50px' }}>
+            <Title level={3} style={{ width: '100%' }}> 현재 진행중인 토론
             </Title>
-            <div style={{ width: '20%', height:'10%', background: '#F0F0F0', display: 'inline-block', margin: '0 auto', padding: '1rem' }}>
-                <h3><strong> 안락사, 허용해야 한다 </strong></h3>
-                <br />
-                <p>2021-05-22 | 16개의 의견 </p>
+            <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center', flexDirection: 'row', flexWrap: 'wrap', width: '100%', minWidth: '60%', paddingLeft: '100px', paddingRight: '100px'}}>
+                <div className="ongoing-discussion" style={{ display: 'flex', alignItems: 'center', width: '200px', height:'150px', background: '#F0F0F0', padding: '1rem', flexWrap: 'wrap', justifyContent: 'center', marginTop: '1rem', marginBottom: '1rem', marginLeft: '0.5rem', marginRight: '0.5rem'}}>
+                    <h3><strong> 안락사, 허용해야 한다 </strong></h3>
+                    <br />
+                    <p>2021-05-22 | 16개의 의견 </p>
+                </div>
             </div>
         </div>
     )
