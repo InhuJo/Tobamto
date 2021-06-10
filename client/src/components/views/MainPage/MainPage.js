0bd669d4c746cc7501254d2368509aa86e6d8cd0
Juyoung Kim <nullyng@gmail.com>
2021-06-10 15:10:16 +0900

    complete Main Page

--- client/src/components/views/MainPage/MainPage.js
+++ client/src/components/views/MainPage/MainPage.js
@@ -17,37 +17,53 @@ function MainPage() {
     };
 
      return (
-        <div>
+        <div style={{ margin: '0 auto', textAlign: 'center' }}>
             { /* 현재 가장 뜨거운 토론 */}
-            <div class="hot-topic" style={{ width: '85%', margin: '6rem auto', textAlign: 'center' }}>
-                <Title level={2} > &#x1F525; 현재 가장 뜨거운 토론 &#x1F525; </Title>
-                <h2> <strong>안락사, 허용해야 한다</strong> </h2>
-                <span style={{backgroundColor: 'lightgray'}}>코뚱땡이님</span>
-                <strong> VS </strong>
-                <span style={{backgroundColor: 'lightgray'}}>궁뎅이님</span>
+            <div className="box" style={{ border: '1px solid lightgrey', margin: '6rem'}}>
+                <div className="hot-topic" style={{ margin: '3rem auto', textAlign: 'center', paddingTop: '3rem', paddingBottom: '3rem' }}>
+                <h3><strong> &#x1F525; 현재 가장 뜨거운 토론 &#x1F525; </strong></h3>
+                <Title level={1}> <i>안락사, 허용해야 한다</i> 
+                </Title>
+                <div className="opinion" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                    <div style={{ width: '30%', backgroundColor: '#B4C7E7', display: 'inline-block', padding: '2rem', margin: '2rem' }}>
+                        <p><strong> &#x1F646; 조궁뎅 님 </strong></p>
+                        <p>고통 없이 죽여준다고? 이거 완전 이득아님?</p>
+                    </div>
+                    <h1 style={{ display: 'inline'}}><strong> VS </strong></h1>
+                    <div style={{ width: '30%', backgroundColor: '#FBE5D6', display: 'inline-block', padding: '2rem', margin: '2rem' }}>
+                        <p className="main-cons"><strong> &#x1F645; 코뚱땡이 님 </strong></p>
+                        <p>솔직히 너네 중에 죽고싶은 사람 있냐고 ㅋㅋㅋ 아 어이없네 ㅋㅋ루삥뽕</p>
+                    </div>
+                </div>
                 <br/>
                 <br/>
                 <a> &#x27A1; 지금 바로 토론하러 가기 </a>
+                </div>
             </div>
+           
 
             { /* 진행 중인 토론 슬라이더 */}
-            <div class="ongoing" style={{ width: '50%', margin: '6rem auto', textAlign: 'center' }}>
-                <Title level={2} > 현재 진행 중인 토론 </Title>
+            <div className="box" style={{ border: '1px solid lightgrey', margin: '6rem'}}>
+                <div className="ongoing" style={{ width: '50%', margin: '3rem auto', textAlign: 'center', paddingTop: '3rem', paddingBottom: '3rem'  }}>
+                <h3><strong> 현재 진행 중인 토론 </strong></h3>
+                <br/>
                 <Slider {...settings}>
                     <div>
-                        <h2 style={{ textAlign: 'center', margin: '2rem auto' }}> <strong><i>1인 미디어, 규제해야 한다</i></strong> </h2>
+                        <h1 style={{ textAlign: 'center', margin: '2rem auto' }}> <strong><i>1인 미디어, 규제해야 한다</i></strong> </h1>
+                        <a href='http://www.naver.com' target="_blank"><u>지금 바로 토론하러 가기</u> </a>
+                    </div>
+                    <div>
+                        <h1 style={{ textAlign: 'center', margin: '2rem auto' }}> <strong><i>코뚱땡이는 바보다</i></strong> </h1>
                         <a href='http://www.naver.com' target="_blank"> <u>지금 바로 토론하러 가기</u> </a>
                     </div>
                     <div>
-                        <h2 style={{ textAlign: 'center', margin: '2rem auto' }}> <strong><i>코뚱땡이는 바보다</i></strong> </h2>
-                        <a href='http://www.naver.com' target="_blank"> <u>지금 바로 토론하러 가기</u> </a>
-                    </div>
-                    <div>
-                        <h2 style={{ textAlign: 'center', margin: '2rem auto' }}> <strong><i>조궁뎅이는 궁뎅이다</i></strong> </h2>
+                        <h1 style={{ textAlign: 'center', margin: '2rem auto' }}> <strong><i>조궁뎅이는 궁뎅이다</i></strong> </h1>
                         <a href='http://www.naver.com' target="_blank"> <u>지금 바로 토론하러 가기</u> </a>
                     </div>
                 </Slider>
+                </div>
             </div>
+            
         </div>
                 
     )
